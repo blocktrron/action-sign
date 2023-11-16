4b190af8454cefc7bf42930a60a49ebf4620cf79
David Bauer <mail@david-bauer.net>
2023-11-16 23:26:57 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+WORKDIR="/gluon/workdir"
+GLUON_DIR="/gluon/gluon-repo"
+
+SIGNING_KEY_PATH="$WORKDIR/signing.key"
+MANIFEST_PATH="$WORKDIR/signing.manifest"
+
+$GLUON_DIR/contrib/sign.sh "$SIGNING_KEY_PATH" "$MANIFEST_PATH"
